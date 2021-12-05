ee82b3519cfafd81bc4f48539b16e48dde2f56aa
MarkeZito3 <MarkkesGames@hotmail.com>
2021-12-04 23:08:50 -0300

    creé todo lo necesario para crear la página con django, menos una base de datos ni modelos ni migraciones :)

---
+++ Proyecto_informatorio/settings/local.py
@@ -0,0 +1,5 @@
+#hice este archivo local para realizar las configuraciones en local sin tener que modificar el archivo settings principal que lo llamé "base.py"
+#para saber porqué hice esto miren este video https://drive.google.com/file/d/1OV8Zo5wAqvD29VwsSz79xwocIkE5fzlo/view
+
+from .base import *
+
---
+++ Proyecto_informatorio/settings/production.py
@@ -0,0 +1,1 @@
+# hice esto asi para cuando el proyecto este en produccion (osea ya en un servidor dedicado)
